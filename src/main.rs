7af3ab378721e47b1f898a19982927db89516627
DeathwingTheBoss <ozcanbarisucar@gmail.com>
2023-03-21 02:06:22 +0300

    - Fix batched calls caching - Limit max request to 512KB - Version bump

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::{time::Duration, sync::Mutex};
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, HttpRequest};
-use serde::{Deserialize, Serialize};
-use reqwest::{Client, ClientBuilder};
-use lru_time_cache::LruCache;
-use serde_json::Value;
-use serde_with::{DurationSeconds, serde_as};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use config::Config;
+use lru_time_cache::LruCache;
+use reqwest::{Client, ClientBuilder};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use serde_with::{serde_as, DurationSeconds};
+use std::{sync::Mutex, time::Duration};
 
-const DRONE_VERSION: &str  = env!("CARGO_PKG_VERSION");
+const DRONE_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 // Drone Cacheable Methods
 const DRONE_CACHEABLE_METHODS: [&str; 7] = [
@@ -19,7 +19,6 @@ const DRONE_CACHEABLE_METHODS: [&str; 7] = [
     "condenser_api.get_block_range",
     "block_api.get_block_range",
 ];
-
 
 #[derive(Serialize, Deserialize, Debug)]
 struct HealthCheck {
@@ -71,6 +70,8 @@ enum Endpoints {
 }
 
 struct APICallResponse {
+    id: u32,
+    jsonrpc: String,
     result: Value,
     cached: bool,
 }
@@ -86,20 +87,20 @@ impl Endpoints {
     }
 }
 
-async fn handle_request(request: &APIRequest, data: &web::Data<AppData>, client_ip: &String ) -> Result<APICallResponse, ErrorStructure> {
+async fn handle_request(
+    request: &APIRequest,
+    data: &web::Data<AppData>,
+    client_ip: &String,
+) -> Result<APICallResponse, ErrorStructure> {
     // Convert the call to a struct.
     let client = data.webclient.clone();
     // If there's a single call, just forward it.
 
     // Get humantime for logging.
     let human_timestamp = humantime::format_rfc3339_seconds(std::time::SystemTime::now());
-    let formatted_log = 
-    format!(
+    let formatted_log = format!(
         "Timestamp: {} || IP: {} || Request Method: {} || Request Params: {}",
-        human_timestamp,
-        client_ip, 
-        request.method, 
-        request.params,
+        human_timestamp, client_ip, request.method, request.params,
     );
     println!("{}", formatted_log);
     // Pick the endpoints depending on the method.
@@ -147,46 +148,57 @@ async fn handle_request(request: &APIRequest, data: &web::Data<AppData>, client_
         _anything_else => Endpoints::HAF,
     };
 
-    // Check if the call is in the cache.
-    if let Some(cached_call) = 
-    data.cache.lock().unwrap().get(&request.params.to_string()) {
-        return Ok(APICallResponse
-         {
-            result: cached_call.clone(),
+    // Check if the call is in the cache. If it is, return only the result while keeping rest of the response the same.
+    if let Some(cached_call) = data.cache.lock().unwrap().get(&request.params.to_string()) {
+        // build result with data from cache and response
+        let result = cached_call.clone();
+        return Ok(APICallResponse {
+            id: request.id,
+            jsonrpc: request.jsonrpc.clone(),
+            result: result["result"].clone(),
             cached: true,
         });
     }
 
     // Send the request to the endpoints.
-    let res = match client.post(endpoints.choose_endpoint(&data))
+    let res = match client
+        .post(endpoints.choose_endpoint(&data))
         .json(&request)
         .send()
-        .await {
-            Ok(response) => response,
-            Err(err) => return Err(ErrorStructure {
+        .await
+    {
+        Ok(response) => response,
+        Err(err) => {
+            return Err(ErrorStructure {
                 code: 1000,
                 message: format!("Unable to send request to endpoint."),
                 error: err.to_string(),
-            }),
-        };
+            })
+        }
+    };
     let body = match res.text().await {
         Ok(text) => text,
-        Err(err) => return Err(ErrorStructure {
-            code: 2000,
-            message: format!("Received an invalid response from the endpoint."),
-            error: err.to_string(),
-        }),
+        Err(err) => {
+            return Err(ErrorStructure {
+                code: 2000,
+                message: format!("Received an invalid response from the endpoint."),
+                error: err.to_string(),
+            })
+        }
     };
     let json_body: serde_json::Value = match serde_json::from_str(&body) {
         Ok(parsed) => parsed,
-        Err(err) => return Err(ErrorStructure {
-            code: 3000,
-            message: format!("Unable to parse endpoint data."),
-            error: err.to_string(),
-        }),
+        Err(err) => {
+            return Err(ErrorStructure {
+                code: 3000,
+                message: format!("Unable to parse endpoint data."),
+                error: err.to_string(),
+            })
+        }
     };
-    if json_body["result"].is_array() && 
-       json_body["result"].as_array().unwrap().is_empty() || json_body["result"].is_null() {
+    if json_body["result"].is_array() && json_body["result"].as_array().unwrap().is_empty()
+        || json_body["result"].is_null()
+    {
         return Err(ErrorStructure {
             code: 4000,
             message: format!("Unable to parse endpoint data."),
@@ -195,16 +207,25 @@ async fn handle_request(request: &APIRequest, data: &web::Data<AppData>, client_
     }
 
     if DRONE_CACHEABLE_METHODS.contains(&request.method.as_str()) {
-        data.cache.lock().unwrap().insert(request.params.to_string(), json_body.clone());
+        data.cache
+            .lock()
+            .unwrap()
+            .insert(request.params.to_string(), json_body.clone());
     }
-    Ok(APICallResponse
-     {
-        result: json_body,
+    Ok(APICallResponse {
+        id: request.id,
+        jsonrpc: request.jsonrpc.clone(),
+        result: json_body["result"].clone(),
         cached: false,
     })
 }
 
-async fn api_call(req: HttpRequest, call: web::Json<APICall>, data: web::Data<AppData>) -> impl Responder {
+async fn api_call(
+    req: HttpRequest,
+    call: web::Json<APICall>,
+    data: web::Data<AppData>,
+) -> impl Responder {
+
     // Log the request, if there's Cloudflare header (CF-Connecting-IP) use that instead of peer_addr.
     let get_cloudflare_ip = req.headers().get("CF-Connecting-IP");
 
@@ -214,21 +235,30 @@ async fn api_call(req: HttpRequest, call: web::Json<APICall>, data: web::Data<Ap
     };
     let user_ip = match client_ip {
         Ok(ip) => ip,
-        Err(_) => return HttpResponse::InternalServerError().json(ErrorStructure {
-            code: 9999,
-            message: "Internal Server Error".to_string(),
-            error: "Invalid Cloudflare Proxy Header.".to_string(),
-        }),
+        Err(_) => {
+            return HttpResponse::InternalServerError().json(ErrorStructure {
+                code: 9999,
+                message: "Internal Server Error".to_string(),
+                error: "Invalid Cloudflare Proxy Header.".to_string(),
+            })
+        }
     };
-    
-    let response = match call.0 {
+
+    match call.0 {
         APICall::Single(request) => {
             let result = handle_request(&request, &data, &user_ip).await;
             match result {
-                Ok(response) => response,
-                Err(err) => return HttpResponse::InternalServerError().json(err),
+                Ok(response) => HttpResponse::Ok()
+                    .insert_header(("Drone-Version", DRONE_VERSION))
+                    .insert_header(("Cache-Status", response.cached.to_string()))
+                    .json(serde_json::json!({
+                        "id": response.id,
+                        "jsonrpc": response.jsonrpc,
+                        "result": response.result,
+                    })),
+                Err(err) => HttpResponse::InternalServerError().json(err),
             }
-        },
+        }
         APICall::Batch(requests) => {
             let mut responses = Vec::new();
             for request in requests {
@@ -244,23 +274,19 @@ async fn api_call(req: HttpRequest, call: web::Json<APICall>, data: web::Data<Ap
                 if response.cached == false {
                     cached = false;
                 }
-                result.push(response.result);
+                result.push(serde_json::json!({
+                    "id": response.id,
+                    "jsonrpc": response.jsonrpc,
+                    "result": response.result,
+                }));
             }
-            APICallResponse
-         {
-                result: serde_json::Value::Array(result),
-                cached: cached,
-            }
-        },
-    };
-
-    HttpResponse::Ok()
-    .insert_header(("Drone-Version", DRONE_VERSION))
-    .insert_header(("Cache-Status", response.cached.to_string()))
-    .json(&response.result)
+            HttpResponse::Ok()
+                .insert_header(("Drone-Version", DRONE_VERSION))
+                .insert_header(("Cache-Status", cached.to_string()))
+                .json(serde_json::Value::Array(result))
+        }
+    }
 }
-
-
 
 struct AppData {
     cache: Mutex<LruCache<String, Value>>,
@@ -288,17 +314,20 @@ struct DroneConfig {
 async fn main() -> std::io::Result<()> {
     // Load config.
     let config = Config::builder()
-    .add_source(config::File::with_name("config.json"))
-    .build()
-    .expect("Could not load config.json")
-    .try_deserialize::<DroneConfig>()
-    .expect("config.json is not in a valid format.");
-    
+        .add_source(config::File::with_name("config.json"))
+        .build()
+        .expect("Could not load config.json")
+        .try_deserialize::<DroneConfig>()
+        .expect("config.json is not in a valid format.");
+
     // Create the cache.
     let _cache = web::Data::new(AppData {
         cache: Mutex::new(
-            LruCache::<String, serde_json::Value>
-            ::with_expiry_duration_and_capacity(config.cache_ttl, config.cache_count)),
+            LruCache::<String, serde_json::Value>::with_expiry_duration_and_capacity(
+                config.cache_ttl,
+                config.cache_count,
+            ),
+        ),
         webclient: ClientBuilder::new()
             .pool_max_idle_per_host(config.actix_connection_threads)
             .build()
@@ -308,7 +337,9 @@ async fn main() -> std::io::Result<()> {
     println!("Drone is running on port {}.", config.port);
     HttpServer::new(move || {
         App::new()
-            .app_data(web::JsonConfig::default().content_type(|_| true))
+            .app_data(web::JsonConfig::default()
+            .content_type(|_| true)
+            .limit(512))
             .app_data(_cache.clone())
             .route("/", web::get().to(index))
             .route("/", web::post().to(api_call))
@@ -317,5 +348,4 @@ async fn main() -> std::io::Result<()> {
     .bind((config.hostname, config.port))?
     .run()
     .await
-    
 }
